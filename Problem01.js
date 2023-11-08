69bf7148ec098c46273a9c543ae20e9f49716bfc
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2023-11-08 16:13:06 +0530

    Update Problem01.js

--- Problem01.js
+++ Problem01.js
@@ -1,6 +1,5 @@
 //Que: Write a simple component called App that displays a button with the text "Click me!" and logs a Clicked! message to the console when clicked.
 const App = () => {
-	// Edit this component
 
 	function log()
 	{
